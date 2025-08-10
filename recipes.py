439a20cea7ccc60d83409b2638e8d983efd48173
Aavani U B <uaavani@gmail.com>
2025-08-10 10:05:27 +0530

    Add files via upload

---
+++ recipes.py
@@ -0,0 +1,39 @@
+from collections import defaultdict, deque
+
+def findAllRecipes(recipes, ingredients, supplies):
+
+    ingredient_to_recipes = defaultdict(list)
+    indegree = {recipe: 0 for recipe in recipes}  
+
+    for recipe, ing_list in zip(recipes, ingredients):
+        for ing in ing_list:
+            ingredient_to_recipes[ing].append(recipe)
+        indegree[recipe] = len(ing_list)
+
+   
+    queue = deque(supplies)
+    result = []
+
+    while queue:
+        item = queue.popleft()
+        
+        for recipe in ingredient_to_recipes[item]:
+            indegree[recipe] -= 1
+            if indegree[recipe] == 0:  
+                result.append(recipe)
+                queue.append(recipe)
+
+    return result
+
+
+
+recipes = ["cake", "frosting", "layered_cake"]
+ingredients = [
+    ["flour", "sugar", "eggs"],        
+    ["sugar", "butter"],              
+    ["cake", "frosting", "strawberries"]  
+]
+supplies = ["flour", "sugar", "eggs", "butter", "strawberries"]
+
+print(findAllRecipes(recipes, ingredients, supplies))
+# Output example: ['bread', 'sandwich', 'burger']
